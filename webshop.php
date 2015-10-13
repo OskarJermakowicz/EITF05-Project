da933aef32c21586b83749bc1777fa1be3c7b8de
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2015-10-13 20:41:23 +0200

    Update webshop.php

--- webshop.php
+++ webshop.php
@@ -12,119 +12,126 @@
 			} 
 
 			.topStyle {
+				margin: 5px;
 				padding: 15px;
-				width: 655px;
-				height: 60px;
+				width: 642px;
+				height: 50px;
 				border: 1px solid black;
+			}
+			
+			.bottomStyle {
+				margin: 5px;
+				padding: 15px;
+				width: 642px;
+				height: 3px;
+				border: 1px solid black;
+			}
+			
+			.floatLeft {
+				float: left;
 			}
 		</style>
 		<title>Webshop</title>
 	</head>
 	
 	<body>
-<?php
-// Connect to server and select databse.
-$db = new PDO('mysql:host=localhost;dbname=db1;charset=utf8', 'root', '');
-?>
+	<?php
+		$db = new PDO('mysql:host=localhost;dbname=db1;charset=utf8', 'root', '');
+	?>
 
 	<div class="topStyle">
 		<h1>Webshop</h1>
 	</div>
 
+	<div class="bottomStyle">
+		<b>Logged in as:</b> USERNAME
+	</div>
+
 	<div class="indexStyle">
   		<h2>Haj</h2>
   		<form method="post">
-		//////Lägg till bild
-9 kr
-					<p>
+		<b>Price:</b> 9 SEK
+		<p>
 			<input name="submitHaj" type="submit" value=" Add to cart ">
 		</p>
-</form>
-<?php
+		</form>
+		<?php
+			if(isset($_REQUEST['submitHaj']))
+			{
+				echo "Added to cart.";
+				$sql = "INSERT INTO Basket (itemName, price)
+				VALUES ('haj', 9)";
+				$db->query($sql);
 
-if(isset($_REQUEST['submitHaj']))
-{
-echo "Added to cart";
-     $sql = "INSERT INTO Basket (itemName, price)
-VALUES ('haj', 9)";
-$db->query($sql);
-
-}
-?>
+			}
+		?>
 	</div>
-<div class="indexStyle">
+	
+	<div class="indexStyle">
   		<h2>Katt</h2>
   		<form method="post">
-		//////Lägg till bild
-4 kr
-					<p>
+		<b>Price:</b> 4 SEK
+		<p>
 			<input name="submitKatt" type="submit" value=" Add to cart ">
 		</p>
-</form>
-
-<?php
-
-if(isset($_REQUEST['submitKatt']))
-{
-echo "Added to cart";
-     $sql = "INSERT INTO Basket (itemName, price)
-VALUES ('Katt', 4)";
-$db->query($sql);
-
-}
-?>
+		</form>
+		<?php
+			if(isset($_REQUEST['submitKatt']))
+			{
+				echo "Added to cart.";
+				$sql = "INSERT INTO Basket (itemName, price)
+				VALUES ('Katt', 4)";
+				$db->query($sql);
+			}
+		?>
 	</div>
-<div class="indexStyle">
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<div class="indexStyle">
   		<h2>Ko</h2>
   		<form method="post">
-		//////Lägg till bild
-5 kr
-					<p>
+		<b>Price:</b> 5 SEK
+		<p>
 			<input name="submitKo" type="submit" value=" Add to cart ">
 		</p>
-</form>
-
-<?php
-
-if(isset($_REQUEST['submitKo']))
-{
-echo "Added to cart";
-     $sql = "INSERT INTO Basket (itemName, price)
-VALUES ('Ko', 5)";
-$db->query($sql);
-
-}
-?>
+		</form>
+		<?php
+			if(isset($_REQUEST['submitKo']))
+			{
+				echo "Added to cart.";
+				$sql = "INSERT INTO Basket (itemName, price)
+				VALUES ('Ko', 5)";
+				$db->query($sql);
+			}
+		?>
 	</div>
-<div class="indexStyle">
+	
+	<div class="indexStyle">
   		<h2>Hund</h2>
   		<form method="post">
-		//////Lägg till bild
-3 kr
-					<p>
+		<b>Price:</b> 3 SEK
+		<p>
 			<input name="submitHund" type="submit" value=" Add to cart ">
 		</p>
-</form>
-
-<?php
-
-if(isset($_REQUEST['submitHund']))
-{
-echo "Added to cart";
-     $sql = "INSERT INTO Basket (itemName, price)
-VALUES ('Hund', 3)";
-$db->query($sql);
-
-}
-?>
+		</form>
+		<?php
+			if(isset($_REQUEST['submitHund']))
+			{
+				echo "Added to cart.";
+				$sql = "INSERT INTO Basket (itemName, price)
+				VALUES ('Hund', 3)";
+				$db->query($sql);
+			}
+		?>
 	</div>
-<form action="logout.php" method="post">
-	<input name="logoutButton" type="submit" value=" Logout">
-</form>
-
-<form action="checkout.php" method="post">
-	<input name="checkoutButton" type="submit" value=" To checkout">
-</form>
-
-	</body>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<div class="floatRight">
+		<form action="logout.php" method="post">
+			<input name="logoutButton" type="submit" value=" Logout">
+		</form>
+		<br>
+		<form action="checkout.php" method="post">
+			<input name="checkoutButton" type="submit" value=" To checkout">
+		</form>
+	</div>
+</body>
 </html>
